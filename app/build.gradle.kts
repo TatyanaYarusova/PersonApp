b3a6f9674ca0ae78a83dfac620c9b28858fdabe6
TatyanaYarusova <tata_yarusova@mail.ru>
2024-02-11 20:21:55 +0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
         jvmTarget = "1.8"
@@ -42,29 +42,46 @@ android {
 }
 
 dependencies {
-    implementation("com.android.volley:volley:1.2.1")
+
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("androidx.core:core-ktx:1.12.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
+    implementation ("androidx.cardview:cardview:1.0.0")
+
+    //coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    //retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.4.0")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation ("androidx.lifecycle:lifecycle-common-java8:2.7.0")
 
+//    implementation("com.android.volley:volley:1.2.1")
+
+//    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.4.0")
+//    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+//    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+//    implementation ("androidx.lifecycle:lifecycle-common-java8:2.7.0")
+
+    //Roop
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 
-    implementation ("io.reactivex.rxjava2:rxandroid:2.1.1")
-    implementation ("io.reactivex.rxjava2:rxjava:2.2.19")
+//    implementation ("io.reactivex.rxjava2:rxandroid:2.1.1")
+//    implementation ("io.reactivex.rxjava2:rxjava:2.2.19")
 
+    //Picasso
     implementation ("com.squareup.picasso:picasso:2.71828")
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+//    implementation("androidx.appcompat:appcompat:1.6.1")
+//    implementation("com.google.android.material:material:1.11.0")
+
 //    implementation("androidx.room:room-common:2.6.1")
-//    implementation("androidx.room:room-ktx:2.6.1")
+
+    implementation ("androidx.work:work-runtime-ktx:2.9.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
